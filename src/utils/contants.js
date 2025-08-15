41093e20d5fc652711a3a979497b68a244c5eb11
rajeshjha2000 <rajeshkumarjha153@gmail.com>
2025-08-15 13:01:55 +0530

    updated gitignore

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -3,7 +3,7 @@ const GOOGLE_API_KEY = "AIzaSyDGvZVApAA2sIobplnT2RGUiHzskJ6CY58";
 export const LIVE_CHAT_COUNT = 25;
 
 export const YOUTUBE_VIDEOS_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc%2Cc0KYU2j0TM4%2CeIho2S0ZahI&key=" +
   GOOGLE_API_KEY;
 
 export const YOUTUBE_SEARCH_API =
